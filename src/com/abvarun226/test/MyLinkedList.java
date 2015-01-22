e0dcd27f52499fab1137a109a04f1be174de8a49
Bharghava A <bharghav@yahoo-inc.com>
2015-01-22 10:35:01 +0530

    Modified LinkedList

--- src/com/abvarun226/test/MyLinkedList.java
+++ src/com/abvarun226/test/MyLinkedList.java
@@ -5,15 +5,22 @@ package com.abvarun226.test;
  */
 
 class Link<T> {
-    public T data1;
-    public Link nextLink;
+    private T data;
+    private Link nextLink;
 
     public Link(T d1) {
-        data1 = d1;
+        data = d1;
+        nextLink = null;
     }
 
-    public void printOut() {
-        System.out.println("Data = " + data1);
+    public T getData() { return data; }
+
+    public void setNextLink(Link<T> nextLink) {
+        this.nextLink = nextLink;
+    }
+
+    public Link<T> getNextLink() {
+        return nextLink;
     }
 
 }
@@ -21,10 +28,11 @@ class Link<T> {
 
 class LinkList<T> {
     private Link<T> first;
-    public static int length=0;
+    private int length;
 
     public LinkList() {
-        first = null;
+        first = new Link(null);
+        length = 0;
     }
 
     public boolean isEmpty() {
@@ -34,35 +42,86 @@ class LinkList<T> {
     public void insert(T d1) {
         length++;
         Link<T> link = new Link<T>(d1);
-        link.nextLink = first;
-        first = link;
+        Link<T> current = first;
+        while (current.getNextLink() != null) {
+            current = current.getNextLink();
+        }
+        current.setNextLink(link);
     }
 
-    public Link delete() {
+    public void insert(T d1, int index) {
+        length++;
+        Link<T> link = new Link<T>(d1);
+        Link<T> current = first;
+        for(int i=1 ; i < index && current.getNextLink() != null ; i++) {
+            current = current.getNextLink();
+        }
+
+        link.setNextLink(current.getNextLink());
+        current.setNextLink(link);
+
+    }
+
+    public T delete(int index) {
+        if(index <= 0 || index > length()) {
+            return null;
+        }
+
         if(this.isEmpty()) {
             System.out.println("Delete Failed! List is Empty!");
             return null;
         }
         length--;
         Link<T> temp = first;
-        first = first.nextLink;
-        return temp;
+        for(int i=1 ; i<index ; i++) {
+            if(temp.getNextLink() == null) {
+                return null;
+            }
+            temp = temp.getNextLink();
+        }
+
+        T output = temp.getNextLink().getData();
+        temp.setNextLink(temp.getNextLink().getNextLink());
+        return output;
     }
 
     public int length() {
         return length;
     }
 
-    public void printList() {
-        Link<T> currentLink = first;
-        System.out.println("List: ");
-        while(currentLink != null) {
-            currentLink.printOut();
-            currentLink = currentLink.nextLink;
+    public T get(int index) {
+        if(index < 0) {
+            return null;
         }
-        System.out.println("");
+        index++;
+        Link<T> currentLink = first;
+        for(int i=1 ; i<=index ; i++) {
+            if(currentLink.getNextLink() == null) {
+                return null;
+            }
+            currentLink = currentLink.getNextLink();
+        }
+        return currentLink.getData();
+
     }
+
+    public String toString() {
+        Link<T> current = first.getNextLink();
+        String output = "[";
+        while (current != null) {
+            output += current.getData().toString();
+            current = current.getNextLink();
+            if(current != null) {
+                output += " , ";
+            }
+        }
+        output += "]";
+        return output;
+    }
+
 }
+
+
 public class MyLinkedList {
 
 	/*
@@ -74,65 +133,64 @@ public class MyLinkedList {
     public static void main(String[] args) {
         LinkList<Integer> list = new LinkList<Integer>();
 
-        System.out.println("Is List empty : " + list.isEmpty() + "\n");
-        list.insert(1);
-        list.insert(3);
-        list.insert(5);
-        list.insert(7);
+        try {
+            System.out.println("Is list empty : " + list.isEmpty() + "\n");
+            list.insert(1);
+            list.insert(3);
+            list.insert(5);
+            list.insert(7);
 
-        System.out.println("Length of List : " + list.length());
+            System.out.println("List list = " + list);
 
-        list.printList();
+            int temp = list.delete(list.length());
 
-        Link temp = list.delete();
-        System.out.print("Deleted Link : ");
-        temp.printOut();
-        System.out.println("Length of List : " + list.length());
+            System.out.println("\nDeleted : " + temp);
+            System.out.println("List list = " + list);
+            System.out.println("Length of list : " + list.length());
 
-        System.out.println("\nIs List empty : " + list.isEmpty() + "\n");
+            System.out.println("\nIs list empty : " + list.isEmpty() + "\n");
 
-        list.printList();
 
-        Link temp1 = list.delete();
-        System.out.println("Length of List : " + list.length());
-        Link temp2 = list.delete();
-        System.out.println("Length of List : " + list.length());
-        Link temp3 = list.delete();
-        System.out.println("Length of List : " + list.length());
-        Link temp4 = list.delete();
-        System.out.println("Length of List : " + list.length());
+            System.out.println("\nDeleted : " + list.delete(list.length()));
+            System.out.println("List list = " + list);
 
-        System.out.println("\n-----------------------------------------\n");
+            System.out.println("\nDeleted : " + list.delete(list.length()));
+            System.out.println("List list = " + list);
 
-        LinkList<String> list1 = new LinkList<String>();
+            System.out.println("\nDeleted : " + list.delete(list.length()));
+            System.out.println("List list = " + list);
 
-        System.out.println("Is List empty : " + list1.isEmpty() + "\n");
-        list1.insert("one");
-        list1.insert("two");
-        list1.insert("three");
-        list1.insert("four");
+            System.out.println("\nDeleted : " + list.delete(list.length()));
+            System.out.println("List list = " + list);
+            System.out.println("Length of List : " + list.length());
 
-        System.out.println("Length of List : " + list1.length());
+            System.out.println("\n-----------------------------------------\n");
 
-        list1.printList();
+            LinkList<String> list1 = new LinkList<String>();
 
-        Link temp5 = list1.delete();
-        System.out.print("Deleted Link : ");
-        temp5.printOut();
-        System.out.println("Length of List : " + list1.length());
+            System.out.println("Is List empty : " + list1.isEmpty() + "\n");
+            list1.insert("one");
+            list1.insert("two");
+            list1.insert("three");
+            list1.insert("four");
 
-        System.out.println("\nIs List empty : " + list1.isEmpty() + "\n");
+            System.out.println("Length of List : " + list1.length());
 
-        list1.printList();
+            System.out.println("List list1 = " + list1);
 
-        list1.delete();
-        System.out.println("Length of List : " + list1.length());
-        list1.delete();
-        System.out.println("Length of List : " + list1.length());
-        list1.delete();
-        System.out.println("Length of List : " + list1.length());
-        list1.delete();
-        System.out.println("Length of List : " + list1.length());
+            list1.delete(list1.length()-2);
+            System.out.println("List list1 = " + list1);
+            list1.delete(1);
+            System.out.println("List list1 = " + list1 );
+            list1.delete(list1.length());
+            System.out.println("List list1 = " + list1 );
+            System.out.println("\nIs List empty : " + list1.isEmpty() + "\n");
+
+            System.out.println("Value at position 0: " + list1.get(0));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
